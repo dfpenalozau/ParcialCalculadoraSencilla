4f3cb7fff68cf789fa8edf9950463df419d89360
estudiantes <estudiantes@labing00>
2017-03-24 16:57:20 -0500

    Se ha creado la interfaz gráfica del programa llamada "Grafica"

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
-/**
- *
- * @author estudiantes
- */
 public class Calculadora {
 
     public static void main(String[] args) {
+        
+        
        
     }
     
