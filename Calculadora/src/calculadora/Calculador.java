8659dec8c6251f083793da531aa2f70dd2ce843d
estudiantes <estudiantes@labing00>
2017-03-24 16:44:42 -0500

    Se ha creado la clase "Calculador" la cual contiene dos atributos; los cuales son las variables donde se van a guardar las variables, tambien contiene cuatro métodos los cuales son las cuatro operaciones básicas.

---
+++ Calculadora/src/calculadora/Calculador.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+public class Calculador {
+
+    public float Numero1;
+    public float Numero2;
+    public float Resultado;
+    
+    public float Sumar(){
+        
+        Resultado=Numero1+Numero2;
+        return Resultado;
+    
+    }
+    
+       public float Restar(){
+        
+        Resultado=Numero1-Numero2;
+        return Resultado;
+    
+    }
+    public float Multiplicar(){
+        
+        Resultado=Numero1*Numero2;
+        return Resultado;
+    
+    }
+    
+       public float Dividir(){
+        
+        Resultado=Numero1/Numero2;
+        return Resultado;
+    
+    }
+}
+
+       
+
+
+
+   
