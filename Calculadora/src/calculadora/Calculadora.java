a195e70e533280c4d6adc4d979f52e5ab293c386
estudiantes <estudiantes@labing00>
2017-03-24 17:03:11 -0500

    Ahora se muestra la interfaz grafica al compilar el programa.

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -5,7 +5,9 @@ public class Calculadora {
 
     public static void main(String[] args) {
         
-        
+        Grafica miGrafica;
+        miGrafica=new Grafica();
+        miGrafica.setVisible(true);
        
     }
     
